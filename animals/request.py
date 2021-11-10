4d84610f06717ed7f608e087c73039b89d9d31fb
Jackie Gregory <jackiebgregory7@gmail.com>
2021-11-09 20:36:48 -0600

    chapter-14 animals with location report

--- animals/request.py
+++ animals/request.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 from models import Animal
-
+from models import Location
 
 ANIMALS = [
     
@@ -23,8 +23,12 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
+            a.customer_id,
+            l.name location_name,
+            l.address location_address
         FROM animal a
+        JOIN Location l
+            ON l.id = a.location_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -34,17 +38,34 @@ def get_all_animals():
         dataset = db_cursor.fetchall()
 
         # Iterate list of data returned from database
+        # for row in dataset:
+
+        #     # Create an animal instance from the current row.
+        #     # Note that the database fields are specified in
+        #     # exact order of the parameters defined in the
+        #     # Animal class above.
+        #     animal = Animal(row['id'], row['name'], row['breed'],
+        #                     row['status'], row['location_id'],
+        #                     row['customer_id'])
+
+        #     animals.append(animal.__dict__)
+        
+        #new for loop from chapter 14
         for row in dataset:
 
-            # Create an animal instance from the current row.
-            # Note that the database fields are specified in
-            # exact order of the parameters defined in the
-            # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
+            # Create an animal instance from the current row
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'], row['location_id'], row['customer_id'])
 
+            # Create a Location instance from the current row
+            location = Location(row['location_id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
+
+            # Add the dictionary representation of the animal to the list
             animals.append(animal.__dict__)
+
+        #end of new for loop from chapter 14
 
     # Use `json` package to properly serialize list as JSON
     return json.dumps(animals)
@@ -113,15 +134,46 @@ def delete_animal(id):
         ANIMALS.pop(animal_index)
 
 
-def update_animal(id, new_animal):
-    # Iterate the ANIMALS list, but use enumerate() so that
-    # you can access the index value of each item.
-    for index, animal in enumerate(ANIMALS):
-        if animal["id"] == id:
-            # Found the animal. Update the value.
-            ANIMALS[index] = new_animal
-            break
+# def update_animal(id, new_animal):
+#     # Iterate the ANIMALS list, but use enumerate() so that
+#     # you can access the index value of each item.
+#     for index, animal in enumerate(ANIMALS):
+#         if animal["id"] == id:
+#             # Found the animal. Update the value.
+#             ANIMALS[index] = new_animal
+#             break
 
+#new update animal from chapter 13
+def update_animal(id, new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                location_id = ?,
+                customer_id = ?
+        WHERE id = ?
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
+
+
+#end of new update animal from chapter 13
 
 def get_animals_by_location(location_id):
 
